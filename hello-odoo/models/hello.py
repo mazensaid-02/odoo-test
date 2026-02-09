56cf25309ae4a5763ab2609abb3fd99eb4ca5af3
mazensaid-02 <mazenbensaid548@gmail.com>
2026-02-09 17:56:00 +0100

    test

---
+++ hello-odoo/models/hello.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class HelloOdoo(models.Model):
+    _name = 'hello.odoo'
+    _description = 'Hello Odoo'
+
+    name = fields.Char(string='Name', required=True)
+    message = fields.Text(string='Message', default='Hello from CI/CD!')
